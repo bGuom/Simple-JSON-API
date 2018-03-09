b12e623696a3e12e4aa7c611f7809892e8b871e7
Buddhi Gunarathna <32662218+bGuom@users.noreply.github.com>
2018-03-10 00:07:07 +0530

    UPDATE and DELETE commands added

---
+++ simpleapi/delete/index.php
@@ -0,0 +1,33 @@
+<?php 
+	session_start();
+
+	// variable declaration
+	$id="";
+	$errors = array();	
+
+	// connect to database
+	$db = mysqli_connect('localhost', 'root', '', 'simpleapi');
+
+	if(isset($_GET['id'])){$id = $_GET['id'];}
+
+	// form validation: ensure that the form is correctly filled
+	if (empty($id)) { array_push($errors, "UserID required"); }
+
+
+
+	// register user if there are no errors in the form
+	if (count($errors) == 0) {
+
+		$query = "DELETE FROM db WHERE ID=$id";
+		mysqli_query($db, $query);
+		$msg = array();
+		array_push($msg, "result:success");
+		echo json_encode($msg);
+
+	}
+
+	if (count($errors) > 0) {echo json_encode($errors);} 
+
+	
+
+?>
---
+++ simpleapi/update/index.php
@@ -0,0 +1,35 @@
+<?php 
+	session_start();
+
+	// variable declaration
+	$id="";
+	$name ="";
+	$errors = array();	
+
+	// connect to database
+	$db = mysqli_connect('localhost', 'root', '', 'simpleapi');
+
+	if(isset($_GET['id'])){$id = $_GET['id'];}
+	if(isset($_GET['name'])){$name = $_GET['name'];}
+
+	// form validation: ensure that the form is correctly filled
+	if (empty($id)) { array_push($errors, "UserID required"); }
+	if (empty($name)) { array_push($errors, "UserName required"); }
+
+
+	// register user if there are no errors in the form
+	if (count($errors) == 0) {
+
+		$query = "UPDATE db SET NAME= '$name' WHERE ID=$id";
+		mysqli_query($db, $query);
+		$msg = array();
+		array_push($msg, "result:success");
+		echo json_encode($msg);
+
+	}
+
+	if (count($errors) > 0) {echo json_encode($errors);} 
+
+	
+
+?>
